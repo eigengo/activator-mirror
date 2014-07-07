36fb16573c4c8fc2f6083306b1e9bcca4d66b6a9
Jan Machacek <jan.machacek@gmail.com>
2014-07-07 17:33:10 +0100

    Renames

--- src/main/scala/org/eigengo/mirror/Main.scala
+++ src/main/scala/org/eigengo/mirror/Main.scala
@@ -15,7 +15,7 @@ object Main extends App with RouteConcatenation {
   implicit val system = ActorSystem("Mirror")
   import system.dispatcher
   val mirror = system.actorOf(Props[MirrorClientActor])
-  val service = system.actorOf(Props(new Main(AuthorisationService.route ~ TimelineService.route(mirror) ~ Ui.route)))
+  val service = system.actorOf(Props(new Main(AuthorisationService.route ~ TimelineService.route(mirror) ~ StaticService.route)))
   val io = IO(Http)
   io ! Http.Bind(service, interface = "0.0.0.0", port = 8080)
 
--- src/main/scala/org/eigengo/mirror/StaticService.scala
+++ src/main/scala/org/eigengo/mirror/StaticService.scala
@@ -3,7 +3,7 @@ package org.eigengo.mirror
 import akka.actor.ActorRefFactory
 import spray.routing.{Route, Directives}
 
-object Ui extends Directives {
+object StaticService extends Directives {
 
   def route(implicit arf: ActorRefFactory): Route = {
     getFromResourceDirectory("")
